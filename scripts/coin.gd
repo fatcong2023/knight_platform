7571a94242089bfa8cf881d33e510afee92d46ab
Chong Jin <ottstormtrooper@gmail.com>
2025-04-29 00:51:58 -0400

    added background

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
 func _ready():
-	print("preparing coin")
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
-
 func _on_body_entered(body: Node2D) -> void:
-	print("this is a coin")
 	if body is Player:
 		print("i picked up a coin !")
+		queue_free()
+
+func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	print("this is a coin 2") # Replace with function body.
